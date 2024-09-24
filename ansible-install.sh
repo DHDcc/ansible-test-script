74f2cb3a2edade2b2200915328b3451aa2aa391d
Tomo <github.gas371@slmail.me>
2024-09-24 18:18:18 +0200

    .

--- ansible-install.sh
+++ ansible-install.sh
@@ -18,28 +18,32 @@ fixParu(){
 main(){
   local repoUrl="https://github.com/DHDcc/${repoName}.git"
   local dependencies=("base-devel" "ansible" "git" "python-psutil")
-  
+
+  changeDirectory(){ cd "$1" &> /dev/null; $SHELL ; }
+
   for dep in "${dependencies[@]}"; do
       if ! command -v "${dep}" &> /dev/null; then
 	     sudo pacman -Syu --noconfirm --needed "${dep}"
       fi
   done
   
-  #fixParu
-  cd $HOME
-  git clone -b "${branchName}" "${repoUrl}" 
-  cd "${playbookDir}" || { >&2 echo "Failed to change directory to ${playbookDir}"; exit 1; }
+  changeDirectory "$HOME"
+  if ! git clone -b "${branchName}" "${repoUrl}"; then 
+         >&2 echo "Failed to clone repo: ${repoName}"
+	 exit 1
+  fi
+
+  changeDirectory "${playbookDir}" || { >&2 echo "Failed to change directory to ${playbookDir}"; exit 1; }
   ansible-galaxy collection install -r requirements.yml
 }
   
 removeForVm(){ sed -i "64d" group_vars/all/vars.yml && sed -i "20,24d" roles/hypervisor/tasks/hypervisor.yml ; }
-changeUserDir(){ cd "${playbookDir}" &> /dev/null; $SHELL ; } 
 
-case "${1}" in
-	--tags) main && removeForVm && ansible-playbook --tags "${tagName}" --ask-become-pass "${playbookName}".yml; changeUserDir
+case "$1" in
+	--tags) main && removeForVm && ansible-playbook --tags "${tagName}" --ask-become-pass "${playbookName}".yml
 		;;
 
-        *) main && removeForVm && ansible-playbook --ask-become-pass "${playbookName}".yml; changeUserDir 
+        *) main && removeForVm && ansible-playbook --ask-become-pass "${playbookName}".yml
 	   ;;
 
 esac
